4a9ca309c9f31eab5f5e72aec2d3c4615d947df7
RubénCano21 <ruben217.rc@gmail.com>
2024-05-10 00:22:09 -0400

    creando el login

---
+++ app/Http/Controllers/LoginController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\cr;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+use Response;
+
+class Login extends Controller
+{
+    public function login(){
+        return view("login");
+    }
+    /**
+     * Display a listing of the resource.
+     */
+    public function index() // read
+    {
+        //
+    }
+
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request) // create
+    {
+        //
+    }
+
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Request $request)
+    {
+        //
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
+use App\Http\Controllers\LoginController;
+
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
-});
+    return view('login');
+})->name('welcome');
+
+Route::get('/login', [LoginController::class,'login']);
