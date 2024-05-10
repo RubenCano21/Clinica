32cec93ce0574f43a4850e9999021e8787dca28d
RubénCano21 <ruben217.rc@gmail.com>
2024-05-09 22:10:01 -0400

    creando usuario y rol en DataBaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\User;
+use App\Models\Rol;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -14,7 +15,19 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         // User::factory(10)->create();
+        User::create(
+            [
+                "name"=> "Administrador",
+                "email"=> "admin@gmail.com",
+                "password"=> bcrypt("12345"),
+            ]
+        );
 
 
+        Rol::create(
+            [
+                "nombre"=> "administrador",
+            ]
+        );        
     }
 }
